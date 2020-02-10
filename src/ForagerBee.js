a1c546d41de293a2e8ef88fd89bb4c72e0ad6287
jon-zawada <jonathan_zawada@yahoo.com>
2020-02-10 15:56:38 -0800

    Adds proper super class access for ForagerBee - was initially set to HoneyMakerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,11 +1,12 @@
 var ForagerBee = function() {
-  HoneyMakerBee.call(this);
+  Bee.call(this);
+  this.age = 10;
   this.job = 'find pollen';
   this.canFly = true;
   this.treasureChest = [];
 };
 
-ForagerBee.prototype = Object.create(HoneyMakerBee.prototype);
+ForagerBee.prototype = Object.create(Bee.prototype);
 
 ForagerBee.prototype.constructor = ForagerBee;
 
